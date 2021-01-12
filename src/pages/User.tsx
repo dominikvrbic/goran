291cd26ec095cd921eaeb445c5bc8689df4a147b
dominikvrbic <dominikvrbic01@gmail.com>
2021-01-12 22:28:12 +0100

    basic search

---
+++ src/pages/User.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+interface Props {}
+
+export const User = (props: Props) => {
+  return <div>user</div>;
+};
---
+++ src/pages/Users.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Input } from '@chakra-ui/react';
+import { useAsync } from 'react-use';
+import { Grid, GridItem, Box } from '@chakra-ui/react';
+import { sUsers } from '../api/github.api';
+
+export const Users = () => {
+  const [search, setsearch] = useState('');
+
+  const { loading, value, error } = useAsync(() => {
+    return sUsers(search);
+  }, [search]);
+  return (
+    <div>
+      users
+      <Input
+        placeholder="Basic usage"
+        onChange={(e) => {
+          setsearch(e.target.value);
+        }}
+      />
+      {value?.total_count}
+      {!loading && (
+        <Grid templateColumns="repeat(12, 1fr)" gap={6}>
+          {value?.items.map((item) => (
+            <GridItem rowSpan={2} colSpan={1} bg="tomato">
+              <Box
+                color="gray.500"
+                fontWeight="semibold"
+                letterSpacing="wide"
+                fontSize="xs"
+                textTransform="uppercase"
+                ml="2"
+              >
+                {item.login}
+              </Box>
+            </GridItem>
+          ))}
+        </Grid>
+      )}
+    </div>
+  );
+};
